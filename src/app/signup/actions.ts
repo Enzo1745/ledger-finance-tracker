ac2129295e35065eba17b50547360c6e8793ee81
Enzo1745 <enzo.pereira7899@gmail.com>
2026-04-21 18:14:47 +0200

    feat: add sign up with email confirmation

---
+++ src/app/signup/actions.ts
@@ -0,0 +1,13 @@
+"use server";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient();
+  const { error } = await supabase.auth.signUp({
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  });
+  if (error) redirect("/signup?error=" + error.message);
+  redirect("/signup?message=Check your email to confirm");
+}
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,10 +1,10 @@
-import { login } from "./actions";
+import { signup } from "./actions";
 
-export default function Login() {
+export default function Signup() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <form action={login}>
+        <form action={signup}>
           <label htmlFor="email">Email:</label>
           <br />
           <input type="text" name="email" placeholder="example@gmail.com" />
@@ -14,7 +14,7 @@ export default function Login() {
           <input type="text" name="password" placeholder="********" />
           <br />
           <button type="submit" className="hover:cursor-pointer">
-            Sign in
+            Sign up
           </button>
         </form>{" "}
       </main>
